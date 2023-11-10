4815dd6a46a9413d05a7510ee845e99581cb6172
AlvaroMV0 <126256653+AlvaroMV0@users.noreply.github.com>
2023-11-09 23:07:41 -0300

    basic logic and forms

--- Tic Tac Toe/scripts.js
+++ Tic Tac Toe/scripts.js
@@ -1,9 +1,36 @@
+var turno = 0;
 
-function myFunction(id) {
-    var x = document.getElementById(id);
-    if (x.style.display === "none") {
-      x.style.display = "block";
-    } else {
-      x.style.display = "none";
-    }
+
+function reset(){
+  var x = document.getElementsByClassName("x");
+  var y = document.getElementsByClassName("y");
+  x.style.display = "none";
+  y.style.display = "none";
+}
+
+
+function determinarTurno(){
+
+}
+
+function show_unshow() {
+  if(turno == 1 ){
+  var x = document.getElementById("x1");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none"; turno --;
+  } 
+} else {
+  var y = document.getElementById("o1");
+  if (y.style.display === "none") {
+    y.style.display = "block";
+  } else {
+    y.style.display = "none";turno ++;
   }
+  
+}
+}
+
+
+reset();
