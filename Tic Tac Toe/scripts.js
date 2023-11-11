8d74aaa76d23a5459f236cbdbb198957d617dcd9
AlvaroMV0 <126256653+AlvaroMV0@users.noreply.github.com>
2023-11-10 23:02:20 -0300

    fix show and unshow, added button and functions

--- Tic Tac Toe/scripts.js
+++ Tic Tac Toe/scripts.js
@@ -1,4 +1,26 @@
 let turno = '';
+let arr = [2][2];
+var t1 = document.getElementById("t1");
+var t2 = document.getElementById("t2");
+var t3 = document.getElementById("t3");
+var t4 = document.getElementById("t4");
+var t5 = document.getElementById("t5");
+var t6 = document.getElementById("t6");
+var t7 = document.getElementById("t7");
+var t8 = document.getElementById("t8");
+var t9 = document.getElementById("t9");
+
+function checkWin(){
+  
+  if(t1 == t2 == t3){
+    if(turno == 'x'){
+      console.log("winns X");
+    } else {
+      console.log("winns O");
+    }
+  }
+}
+
 
 
 function reset(){
@@ -20,20 +42,26 @@ function determinarTurno(){
 }
 
 function show_unshow(i) {
+  var y = document.getElementById("o"+i);
+  var x = document.getElementById("x"+i);
   if(x.style.display == "none" && y.style.display == "none"){
     if(turno == 'x' ){
-        var x = document.getElementById("x"+i);
-        if (x.style.display == "none") {
-        x.style.display = "block";
-        }
+        if (x.style.display == "none") {  x.style.display = "block";  writeDiv(i);  }
     } else {
-        var y = document.getElementById("o"+i);
-        if (y.style.display == "none") {
-        y.style.display = "block"; 
-        } 
+        if (y.style.display == "none") {  y.style.display = "block";  writeDiv(i);  } 
     }
     determinarTurno();
+    checkWin();
   }
+}
+
+function writeDiv(i){
+    var t = document.getElementById("t"+i);
+    if(turno == 'x'){
+      t = 'x';
+    } else {
+      t = 'o';
+    }
 }
 
 
