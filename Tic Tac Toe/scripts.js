92aca46a6fb887688e26f9a02fa7e8584db5ae84
AlvaroMV0 <126256653+AlvaroMV0@users.noreply.github.com>
2023-11-12 18:30:02 -0300

    game logic works

--- Tic Tac Toe/scripts.js
+++ Tic Tac Toe/scripts.js
@@ -1,5 +1,5 @@
 let turno = '';
-let arr = [2][2];
+var arr = [['1','2','3'],['4','5','6'],['7','8','9']];
 var t1 = document.getElementById("t1");
 var t2 = document.getElementById("t2");
 var t3 = document.getElementById("t3");
@@ -12,15 +12,23 @@ var t9 = document.getElementById("t9");
 
 function checkWin(){
   
-  if(t1.innerHTML == t2.innerHTML == t3){
-    if(turno == 'x'){
-      console.log("winns X");
-    } else {
-      console.log("winns O");
+  if((arr[0][0] == arr[1][1] && arr[1][1] == arr[2][2]) ||
+  (arr[0][2] == arr[1][1] && arr[1][1] == arr[2][0])){
+    wins();
+    return;
+  }else{
+    for (let j = 0 ; j < 3 ; j++){
+        if((arr[j][0] == arr[j][1] && arr[j][1] == arr[j][2]) ||
+        (arr[0][j] == arr[1][j] && arr[1][j] == arr[2][j])) {
+          wins();
+          return;
+      }
     }
   }
 }
-
+function wins(){
+  if(turno == 'x'){ console.log("winns X"); } else { console.log("winns O"); }
+}
 
 
 function reset(){
@@ -29,7 +37,7 @@ function reset(){
     var y = document.getElementById("o"+i);
     x.style.display = "none";
     y.style.display = "none";
-  }
+  } 
 }
 
 
@@ -50,11 +58,47 @@ function show_unshow(i) {
     } else {
         if (y.style.display == "none") {  y.style.display = "block";  } 
     }
-    determinarTurno();
+    upadteBoard(i);
     checkWin();
+    determinarTurno();
   }
+}
+
+function upadteBoard(i){
+  
+  switch(i){
+    
+    case 1:
+      if(turno == 'x'){arr[0][0] = 'x' ;} else {arr[0][0] = 'o';}
+      break;
+    case 2:
+      if(turno == 'x'){arr[0][1] = 'x' ;} else {arr[0][1] = 'o';} 
+      break;
+    case 3:
+      if(turno == 'x'){arr[0][2] = 'x' } else {arr[0][2] = 'o';}
+      break;
+    case 4:
+      if(turno == 'x'){arr[1][0] = 'x' ;} else {arr[1][0] = 'o';} 
+      break;
+    case 5:
+      if(turno == 'x'){arr[1][1] = 'x' ;} else {arr[1][1] = 'o';} 
+      break;
+    case 6:
+      if(turno == 'x'){arr[1][2] = 'x' ;} else {arr[1][2] = 'o';} 
+      break;
+    case 7:
+      if(turno == 'x'){arr[2][0] = 'x' ;} else {arr[2][0] = 'o';} 
+      break;
+    case 8:
+      if(turno == 'x'){arr[2][1] = 'x' ;} else {arr[2][1] = 'o';} 
+      break;
+    case 9:
+      if(turno == 'x'){arr[2][2] = 'x'  ;} else {arr[2][2] ='o';} 
+      break; 
+
+  }
+
 }
 
 
 reset();
-
