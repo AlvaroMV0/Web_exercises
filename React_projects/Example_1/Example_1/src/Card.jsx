64968eb2cd6feefe7034eeeee26a493570522aa2
AlvaroMV0 <126256653+AlvaroMV0@users.noreply.github.com>
2024-01-01 23:16:50 -0300

    ..

---
+++ React_projects/Example_1/Example_1/src/Card.jsx
@@ -0,0 +1,16 @@
+
+
+
+function Card(){
+    return(
+        <div className="card-container">
+        <div className="card">
+        <img className="card-image" src="https:/via.placeholder.com/150" alt="person" />
+        <h2> Bro Code </h2>
+        <p> Content creator </p>
+        </div>
+        </div>
+    );
+}
+
+export default Card
